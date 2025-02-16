70c0816c8fd57ab8e86ab48b1eb4b8f638576573
Leonardo P. Monteiro <dev.pmonteiro@gmail.com>
2025-02-15 22:17:40 -0300

    URLs
    Created URLs and views.

---
+++ produto/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'produto'
+
+urlpatterns = [
+    path('', views.ListaProdutos.as_view(), name='lista'),
+    path('adicionaraocarrinho/', views.AdicionarAoCarrinho.as_view(), name='adicionaraocarrinho'),
+    path('RemoverDoCarrinho/', views.RemoverDoCarrinho.as_view(), name='RemoverDoCarrinho'),
+    path('Carrinho/', views.Carrinho.as_view(), name='Carrinho'),
+    path('finalizar/', views.Finalizar.as_view(), name='finalizar'),
+    path('slug/', views.DetlheProdutos.as_view(), name='detalhe'),
+] 
--- produto/views.py
+++ produto/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views import View
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+class ListaProdutos(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Listar Produtos')
+    
+class DetlheProdutos(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Detalhes do Produto')
+    
+class AdicionarAoCarrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Adicionar ao Carrinho')
+    
+class RemoverDoCarrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Remover do Carrinho')
+    
+class Carrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Carrinho')
+    
+class Finalizar(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Finalizar')
