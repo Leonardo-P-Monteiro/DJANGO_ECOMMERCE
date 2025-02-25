c8e0dfb06c2c7379e75b4710e87f6318481d8d38
Leonardo P. Monteiro <dev.pmonteiro@gmail.com>
2025-02-25 20:19:45 -0300

    CART FINALIZED

--- produto/views.py
+++ produto/views.py
@@ -156,7 +156,7 @@ class Carrinho(View):
         return render(self.request, 'produto/carrinho.html')
         
     
-class Finalizar(View):
+class ResumoDaCompra(View):
     def get(self, *args, **kwargs):
         return HttpResponse('Finalizar')
 
